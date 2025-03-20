070e4c289cf7d9c90ae155ed5ca2e7e586175901
Felix <felix.lobo85@gmail.com>
2025-03-19 21:07:01 -0400

    Pratica de Listas

---
+++ praticaListas.py
@@ -0,0 +1,10 @@
+frutas = ["maca", "laranja", "banana", "cereja"]
+
+##Sinal de + usado para concatenar strings
+
+print([1, 2] + [3, 4]) 
+print(frutas + [6, 7, 8, 9])
+
+##Sinal de * usado para fazer uma string se repetir quantas vezes requisitado
+print(["teste"] * 4)
+print([1, 2, ["Olá", "Adeus"]] * 2)
