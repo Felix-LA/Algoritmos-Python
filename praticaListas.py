8304c61c43a25b058ae6becc01c00fda3abfc54c
Felix <felix.lobo85@gmail.com>
2025-03-19 21:09:46 -0400

    Update praticaListas.py

--- praticaListas.py
+++ praticaListas.py
@@ -8,3 +8,5 @@ print(frutas + [6, 7, 8, 9])
 ##Sinal de * usado para fazer uma string se repetir quantas vezes requisitado
 print(["teste"] * 4)
 print([1, 2, ["Olá", "Adeus"]] * 2)
+
+##Coloque sempre dentro de [], principalmente quando se usa numeros, para nao se usar a função matematica correspondente
