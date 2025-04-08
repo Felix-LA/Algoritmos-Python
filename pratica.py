a8af84f15107213bcc369e3b79abbff71e64db7d
Felix <felix.lobo85@gmail.com>
2025-04-08 11:26:20 -0400

    Update pratica.py

--- pratica.py
+++ pratica.py
@@ -1,4 +1,4 @@
-print("-- Reservatório de Água --") #Reservatorio esta escrito errado, faltou fechar o nparenteses
+print("-- Reservatório de Água --") #Reservatorio esta escrito errado, faltou fechar o parenteses
 
 altura = float(input(" Digite a altura (cm):"))
 largura = int(input(" Digite a largura (cm): ")) # faltou fechar o parenteses
@@ -8,11 +8,11 @@ c_diario = float(input("Digite o valor do consumo médio diário(litros/dia)= ")
 cap_total = ((altura * largura * comprimento)/1000); '''o resultado seria em cm3 por isso, dividimos por mil para passar de cm3 para litros''' # Colocou virgula dentro do calculo
 auton_reser = cap_total / c_diario
 
-print("Capacidade do Reservatório = ",cap_total, "litros ")
-print("Autonomia do reservatório = ",auton_reser," dias") '''Agora, vamos classificar o consumo'''
+print("Capacidade do Reservatório = ",cap_total, "litros ") 
+print("Autonomia do reservatório = ",auton_reser," dias"); '''Agora, vamos classificar o consumo''' # faltou a virgula para o comentario
 if(auton_reser < 2):
-print("Consumo Elevado")
-if(auton_reser >=2 and auton_reser = 7): # faltou o símbolo '=' estava elif invez de if
+     print("Consumo Elevado") #faltava identação
+elif(auton_reser >= 2 and auton_reser == 7): # faltou o símbolo '=='
      print("Consumo Moderado")
-if(auton_reser > 7): #estava elif invez de if
+elif(auton_reser > 7): #estava elif invez de if
 	print("\n Consumo Baixo") #Identação
