ccd72497dda4d56623795a3c78c1e66ac7cc8e33
Felix <felix.lobo85@gmail.com>
2025-05-01 23:40:11 -0400

    Terminando Att29

--- exercicio29.py
+++ exercicio29.py
@@ -1,9 +1,9 @@
 nomeProduto = ['Caderno', 'Caneta', 'Lapis', 'Borracha', 'Regua']
-codigoProduto = [0, 1, 2, 3, 4, 5] 
+codigoProduto = [0, 1, 2, 3, 4] 
 quantidadeProduto = [10, 10, 10, 10, 10,]
 
-
 while True:
+    
     print("-----MENU-----")
     print("E - Entrada no Estoque")
     print("S - Saida no Estoque")
@@ -30,7 +30,7 @@ while True:
         acaoEstoque = int(input("Informe o Codigo do Produto: "))
         if acaoEstoque in codigoProduto:
             retirada = int(input("Informe a Quantidade a ser retirada: "))
-            if retirada > quantidadeProduto[acaoEstoque]:
+            if retirada < quantidadeProduto[acaoEstoque]:
                 quantidadeProduto[acaoEstoque] =+ quantidadeProduto[acaoEstoque] - retirada
             else: print("A quantidade a ser retirada é menor que no estoque")
     
@@ -38,6 +38,14 @@ while True:
         print(nomeProduto)
         print(codigoProduto)
         print(quantidadeProduto)
+
+    elif acao == "C":
+        novoProduto = input("Informe o nome do produto: ")
+        quantidadeNovoProduto = int(input("Informe a Quantidade do Novo Produto: "))
+        nomeProduto.append(novoProduto)
+        quantidadeProduto.append(quantidadeNovoProduto)
+        codigoNovoProduto = max(codigoProduto) + 1
+        codigoProduto.append(codigoNovoProduto)
 
     elif acao == "S":
         print("Ate Mais")
