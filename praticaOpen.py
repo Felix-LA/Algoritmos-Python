c7d9f6697f3eb8ac2f8c06e2a8afa2ff81c38bd7
Felix <felix.lobo85@gmail.com>
2025-05-26 21:48:49 -0400

    praticas open

---
+++ praticaOpen.py
@@ -0,0 +1,55 @@
+
+#cria um arquivo se nao existir
+#Forma de abrir arquivos
+# passe uma variavel para salvar o arquivo
+# a = open("file.txt", "r")
+
+
+#Forma de ler o arquivo
+# print(a.read())
+
+
+#Fechar arquivo
+# a.close()
+
+#Forma de adicionar conteudo
+# a de apend, para adicionar conteudo
+# b = open("file.txt", "a")
+# b.write("Adicionando Conteudo")
+# b = open("file.txt", "r")
+# print(b.read())
+# b.close()
+
+# # pode passar uma variavel na hora de adicionar
+# # use o utf-8 par a formatação, e para permitir que passe acentos,etc...
+# c = open("file.txt", "a", encoding = "utf-8")
+# x = input("Informe o seu Nome: ")
+# c.write(x)
+# #para pular linha passe um 
+# c.write("\n")
+# c = open("file.txt", "r")
+# print(c.read())
+# c.close()
+
+# #forma de subescrever um programa
+# #cria um arquivo se nao existir
+# #se ja existir conteudo ele ira sobre-escrever o conteudo
+# d = open("file.txt", "w")
+# d.write("aaaaa")
+# d = open("file.txt", "r")
+# print(d.read())
+# d.close()
+
+
+#so cria o arquivo
+# da erro se o arquivo ja existir
+# e = open("file.txt", "x")
+
+# #pode-se especificar como eu quero que ele seja lido
+# #para texto
+# b = open("file.txt", "rt")
+# #para binario
+# # bastante usado para imagens
+# b = open("file.txt", "rb")
+
+
